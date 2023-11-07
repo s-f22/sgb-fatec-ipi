b6728f40a546c11992cfaf198ad2cd57a4a8af60
karamazov <samuel.silveira01@fatec.sp.gov.br>
2023-11-06 22:33:21 -0300

    pg incial OK + dashboard

--- front_end/src/Welcome.js
+++ front_end/src/Welcome.js
@@ -1,37 +1,50 @@
 import { Container, Button, Row, Col } from "react-bootstrap";
-import Login from "./components/LogIn"
+import Login from "./components/LogIn";
 import SignUp from "./components/SignUp";
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-import logo from './logo.png';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import logo from "./logo.png";
 
 function Welcome() {
   const navigate = useNavigate();
   const { user, isAuthenticated, isLoading } = useAuth0();
-  
+
   useEffect(() => {
-    isAuthenticated &&
-      navigate("/sgb");
-      // console.log(user.sub)
+    isAuthenticated && navigate("/sgb");
+    // console.log(user.sub)
   }, [isAuthenticated, navigate]);
-  
+
   return (
-    <Container className="text-center">
-      <Row className="justify-content-center align-items-center" style={{ minHeight: '100vh'}}>
-        <Col md={6} className="texto">
-          <img style={{width: 260, height:268 }} src={logo} alt="FATEC Logo" className="img-fluid mb-4" />
-          <p className="mb-6" style={{width: 209, height:69, fontSize:15, fontWeight: 'bold', textAlign: 'justify', color: '#345059',}}>
-          Bem vinda ao Sistema de Gerenciamento de Bancas da Fatec Ipiranga!          
-          </p>
-          
-          <div style={{ gap: 10 }} className="d-flex justify-content-center mb-4">
-            <Login />
-            <SignUp />
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <div
+      className="justify-content-center align-items-center"
+      style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+    >
+      <img
+        style={{ width: 260, height: 268 }}
+        src={logo}
+        alt="FATEC Logo"
+        className="img-fluid mb-4"
+      />
+      <p
+        className="mb-6"
+        style={{
+          width: 209,
+          height: 69,
+          fontSize: 15,
+          fontWeight: "bold",
+          textAlign: "justify",
+          color: "#345059",
+        }}
+      >
+        Bem vinda ao Sistema de Gerenciamento de Bancas da Fatec Ipiranga!
+      </p>
+
+      <div style={{ gap: 10 }} className="d-flex justify-content-center mb-4">
+        <Login />
+        <SignUp />
+      </div>
+    </div>
   );
 }
 
--- front_end/src/components/Painel.jsx
+++ front_end/src/components/Painel.jsx
@@ -2,14 +2,28 @@ import React, { useEffect, useState } from "react";
 import dashboard from "../assets/img/dashboard.png";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
-import Spinner from 'react-bootstrap/Spinner';
+import { useNavigate } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
+import { Typography, Grid, Paper, Container } from "@mui/material";
 
 const Painel = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
   const [professor, setProfessor] = useState({});
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+
+  const data = {
+    numProfessores: 35,
+    numAlunos: 750,
+    numTemasCadastrados: 120,
+    numTemasDisponiveis: 80,
+    numTrabalhosAndamento: 25,
+    numTrabalhosConcluidos: 50,
+    numBancasAgendadas: 10,
+    notaMediaTrabalhos: 8.2,
+    periodoTrabalhos: "2023",
+    trabalhosPorSemestre: "15, 20, 30, 10"
+  };
 
   useEffect(() => {
     if (!isLoading) {
@@ -41,24 +55,59 @@ const Painel = () => {
   return (
     <div className="Painel_Container">
       {loading ? (
-        <div className="App_MainContent">
-          <Spinner style={{ alignSelf: 'center' }} animation="grow" />
-          <p>Carregando...</p>
+        <div
+          style={{
+            display: "flex",
+            flex: 1,
+            height: "100vh",
+            flexDirection: "column",
+            alignContent: "center",
+            justifyContent: "center",
+          }}
+        >
+          <p style={{ alignSelf: "center" }}>Carregando...</p>
+          <Spinner
+            style={{ alignSelf: "center", marginBottom: "50px" }}
+            animation="grow"
+          />
         </div>
+      ) : professor.email_inst_verif === true ? (
+        <>
+          <Container maxWidth>
+            <Typography variant="h4" gutterBottom>
+              Painel de Controle
+            </Typography>
+            <Grid container spacing={3}>
+              {Object.keys(data).map((key) => (
+                <Grid item xs={12} sm={6} md={3} key={key}>
+                  <Paper
+                    style={{
+                      padding: "16px",
+                      textAlign: "center",
+                      color: "text.primary",
+                      boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+                    }}
+                  >
+                    <Typography variant="h6">{key}</Typography>
+                    <Typography variant="h4">{data[key]}</Typography>
+                  </Paper>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </>
       ) : (
-        professor.email_inst_verif === true ? (
-          <>
-            <h1>Painel Geral</h1>
-            <img style={{ borderRadius: 30 }} src={dashboard} alt="" />
-          </>
-        ) : (
-          <div>
-            <h2 style={{ width: '50%' }}>Bem vindo ao SGB-Fatec-Ipiranga!</h2>
-            <h3 style={{ width: '50%', marginTop: '30px' }}>Para ter acesso completo às funcionalidades do sistema, por favor, <b>verifique seu e-mail</b> e clique no link de ativação.</h3>
-            <h4 style={{ marginTop: '30px' }}>Caso não tenha recebido o link, entre em contato com o suporte:</h4>
-            <a href="">algumEmailDeSuporte@fatec.sp.gov.br</a>
-          </div>
-        )
+        <div>
+          <h2 style={{ width: "50%" }}>Bem vindo ao SGB-Fatec-Ipiranga!</h2>
+          <h3 style={{ width: "50%", marginTop: "30px" }}>
+            Para ter acesso completo às funcionalidades do sistema, por favor,{" "}
+            <b>verifique seu e-mail</b> e clique no link de ativação.
+          </h3>
+          <h4 style={{ marginTop: "30px" }}>
+            Caso não tenha recebido o link, entre em contato com o suporte:
+          </h4>
+          <a href="">algumEmailDeSuporte@fatec.sp.gov.br</a>
+        </div>
       )}
     </div>
   );
